94d7bfafb764c6d8a1518aef10cffeaca82f0116
yosu <woodstock830@gmail.com>
2018-09-24 18:14:41 +0900

    Implement mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Set R2 to 0
+    @R2
+    M=0
+(Start)
+    // Load R0
+    @R0
+    D=M
+
+    // End if R0=0
+    @End
+    D;JEQ
+
+    // R2 += R1
+    @R1
+    D=M
+    @R2
+    M=D+M
+
+    // R0 -= 1
+    @R0
+    M=M-1
+    @Start
+    0;JMP
+(End)
+    @End
+    0;JMP
